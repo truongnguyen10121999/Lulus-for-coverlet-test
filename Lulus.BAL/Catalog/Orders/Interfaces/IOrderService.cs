50d422191d4d814a3c10e0ea4cf63298a673981d
Linh Mac Le My <67138256+Maruhara18774@users.noreply.github.com>
2021-10-07 13:55:44 +0700

    Add order interface

---
+++ Lulus.BAL/Catalog/Orders/Interfaces/IOrderService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lulus.BAL.Catalog.Orders.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<bool> GetCurrentOrderAsync();
+        Task<bool> AddProductAsync();
+        Task<bool> RemoveProductAsync();
+        Task<bool> ClearCartAsync();
+        Task<bool> ChangeQuantityAsync();
+        Task<bool> GetCheckoutInforAsync();
+        Task<bool> CheckoutAsync();
+    }
+}
---
+++ Lulus.BAL/Catalog/Orders/OrderService.cs
@@ -0,0 +1,18 @@
+﻿using Lulus.Data.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lulus.BAL.Catalog.Orders
+{
+    public class OrderService
+    {
+        private readonly LulusDBContext _context;
+        public OrderService(LulusDBContext context)
+        {
+            _context = context;
+        }
+    }
+}
